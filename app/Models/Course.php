5d0ecb6b27ab088e409cbc412c55931c9c6619a7
patriciadurango <patriciadurango24@gmail.com>
2026-03-09 21:46:13 -0600

    Activity 7

---
+++ app/Models/Course.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+public function kit() {
+    return $this->belongsTo(Kit::class, 'id_kit');
+}
+}
